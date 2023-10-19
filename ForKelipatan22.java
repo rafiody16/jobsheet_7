9383cf9dd5d177bee8326cd1bb1208293bd06aaa
rafiody16 <rafiody16@gmail.com>
2023-10-19 10:34:15 +0700

    add rata-rata

--- ForKelipatan22.java
+++ ForKelipatan22.java
@@ -8,7 +8,7 @@ public class ForKelipatan22 {
     {
         Scanner scan = new Scanner(System.in);
 
-        int kelipatan, jumlah = 0, counter = 0;
+        int kelipatan, jumlah = 0, counter = 0, rata2 = 0;
 
         System.out.println("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
@@ -19,9 +19,11 @@ public class ForKelipatan22 {
             {
                 jumlah += i;
                 counter ++;
+                rata2 = jumlah/counter;
             }
         }
         System.out.printf("Banykanya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata dari seluruh bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rata2);
     }
 }
